90ac47e8fb247e6b18658b3dfbcc29ca81ce14fb
anoff <offenhaeuser@gmail.com>
2017-01-02 23:21:30 +0100

    :ant: fix(car): heading calculation

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const MAX_SIZE = 600;
-
+const STREET_WIDTH = 10;
 let intersections = [
   // 100 is max (autoscaled)
   [0,0],
@@ -32,18 +32,20 @@ const segments = [
   [4, 6]
 ];
 function createStreet(graphics) {
-  graphics.lineStyle(10, 0xffd900, 1);
+  graphics.lineStyle(STREET_WIDTH, 0xffd900, 1);
   segments.forEach(elm => {
     const start = intersections[elm[0]];
     const end = intersections[elm[1]];
     graphics.moveTo(start[0], start[1]);
     graphics.lineTo(end[0], end[1]);
   });
+  intersections.forEach(elm => {
+    graphics.drawCircle(elm[0], elm[1], STREET_WIDTH);
+  });
 }
 
 function randomPos() {
-  let segment = segments[Math.floor(Math.random * segments.length)];
-  segment = segments[0];
+  const segment = segments[Math.floor(Math.random() * segments.length)];
   const start = intersections[segment[0]];
   const end = intersections[segment[1]];
   const x = end[0] - start[0];
@@ -52,8 +54,10 @@ function randomPos() {
 
   const xD = Math.random() * x;
   const yD = y * xD / x;
-  const heading = Math.acos(Math.cos(Math.abs(x), hyp));
-
+  let heading = Math.atan2(y, x) + Math.PI / 2;
+  if (xD / x > 0.5) {
+    heading = heading + Math.PI;
+  }
   return {
     x: start[0] + xD,
     y: start[1] + yD,
@@ -80,7 +84,6 @@ class MainState {
     createStreet(this.street);
 
     const newPos = randomPos();
-    console.log(newPos);
     const newCar = game.add.sprite(newPos.x, newPos.y, 'car');
     newCar.anchor.setTo(0.5, 0.5);
     newCar.width = this.carSize;
@@ -89,6 +92,12 @@ class MainState {
     newCar.rotation = newPos.heading;
 
     game.physics.arcade.enable(newCar);
+    const VEL = 20;
+
+    // TODO fix velocity orientation
+    newCar.body.velocity.y = -VEL / Math.sin(newCar.rotation);
+    newCar.body.velocity.x = VEL /  Math.cos(newCar.rotation);
+    console.log(newCar);
   }
 
   update() {
