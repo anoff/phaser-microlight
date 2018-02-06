14d8193b0113b1498b3b63f420aec3b747884962
anoff <offenhaeuser@gmail.com>
2018-02-06 21:17:20 +0100

    :star2: feat(world): log as geojson for debugging
    undefined

--- map-import.js
+++ map-import.js
@@ -6,7 +6,7 @@ const Osm2Obj = require('osm2obj')
 const toArray = require('stream-to-array')
 
 const filepath = path.normalize(path.join(__dirname, './marbach.osm'))
-console.log(filepath)
+// console.log(filepath)
 
 function getElements (filepath, filterFn) {
   return new Promise((resolve, reject) => {
@@ -26,9 +26,9 @@ function getElements (filepath, filterFn) {
 }
 
 let ways
-getElements(filepath, d => d.type === 'way' && (d.tags || {}).highway === 'residential')
+getElements(filepath, d => d.type === 'way' && (d.tags || {}).highway === 'secondary')
 .then(w => {
-  w = w.filter(e => e.nodes.length === 2)
+  // w = w.filter(e => e.nodes.length === 2) // filter for ways with only 2 points
   const wayNodes = w
   .map(e => e.nodes)
   .reduce((p, c) => {
@@ -37,21 +37,47 @@ getElements(filepath, d => d.type === 'way' && (d.tags || {}).highway === 'resid
   }, [])
   const wayNodeSelectorFn = d => d.type === 'node' && wayNodes.indexOf(d.id) > -1
   ways = w
+  // get all nodes that are on the selected ways
   return getElements(filepath, wayNodeSelectorFn)
-})
-.then(nodes => {
-  ways = ways.map(w => {
-    const pos = w.nodes.map(wn => {
-      const node = nodes.find(n => n.id === wn)
-      if (!node) {
-        console.error(`no node information found for ${wn}`)
-        return {lat: undefined, lon: undefined}
-      } else {
-        return {lat: node.lat, lon: node.lon}
-      }
+  .then(nodes => {
+    ways = ways.map(w => {
+      // create an array of node positions {lat, lon} for each way
+      const pos = w.nodes.map(wn => {
+        const node = nodes.find(n => n.id === wn)
+        if (!node) {
+          console.error(`no node information found for ${wn}`)
+          return {lat: undefined, lon: undefined}
+        } else {
+          return {lat: node.lat, lon: node.lon}
+        }
+      })
+      w.coordinates = pos
+      return w
     })
-    w.position = pos
-    return w
+    return ways
   })
-  console.log(ways)
+})
+.then(ways => {
+  const features = ways
+  .map(w => {
+    const lineString = {
+      'type': 'Feature',
+      'properties': {
+        'stroke': '#f0f',
+        'stroke-width': 5
+      },
+      'geometry': {
+        'type': 'LineString',
+        'coordinates': []
+      }
+    }
+
+    lineString.geometry.coordinates = w.coordinates.map(c => [c.lon, c.lat])
+    return lineString
+  })
+  const geojson = {
+    'type': 'FeatureCollection',
+    'features': features
+  }
+  console.log(JSON.stringify(geojson))
 })
