4266c0e5e52a0912adcde33115a93a660ba6add2
anoff <offenhaeuser@gmail.com>
2018-02-06 20:11:56 +0100

    :scissors: refactor: change to standard style
    undefined

--- main.js
+++ main.js
@@ -1,44 +1,44 @@
 /* eslint-disable import/no-extraneous-dependencies, import/no-unresolved, import/no-unassigned-import */
-import 'pixi';
-import 'p2';
+import 'pixi'
+import 'p2'
 /* eslint-enable */
-import Phaser from 'phaser';
-import config from './lib/config';
-import CarManager from './lib/carmanager';
-import {createMap} from './lib/street';
-import {loadSprites as loadCars} from './assets/cars';
+import Phaser from 'phaser'
+import config from './lib/config'
+import CarManager from './lib/carmanager'
+import {createMap} from './lib/street'
+import {loadSprites as loadCars} from './assets/cars'
 
 // Initialize Phaser
-const game = new Phaser.Game(config.GAME_SIZE, config.GAME_SIZE);
-global.game = game;
+const game = new Phaser.Game(config.GAME_SIZE, config.GAME_SIZE)
+global.game = game
 
 // Create our 'main' state that will contain the game
 class MainState {
-	preload() {
-		loadCars(game);
-		this.graphics = game.add.graphics(0, 0);
-		this.carManager = new CarManager(game);
-	}
+  preload () {
+    loadCars(game)
+    this.graphics = game.add.graphics(0, 0)
+    this.carManager = new CarManager(game)
+  }
 
-	create() {
-		game.stage.backgroundColor = '#ececec';
-		game.physics.startSystem(Phaser.Physics.ARCADE);
-		createMap(this.graphics);
-		const spaceKey = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
-		spaceKey.onDown.add(this.carManager.addCar.bind(this.carManager));
-		// init the world with 3 cars
-		for (let i = 3; i > 0; i--) {
-			this.carManager.addCar();
-		}
-	}
+  create () {
+    game.stage.backgroundColor = '#ececec'
+    game.physics.startSystem(Phaser.Physics.ARCADE)
+    createMap(this.graphics)
+    const spaceKey = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR)
+    spaceKey.onDown.add(this.carManager.addCar.bind(this.carManager))
+    // init the world with 3 cars
+    for (let i = 3; i > 0; i--) {
+      this.carManager.addCar()
+    }
+  }
 
-	update() {
-		this.carManager.update();
-	}
+  update () {
+    this.carManager.update()
+  }
 }
 
 // Add the 'mainState' and call it 'main'
-game.state.add('main', MainState);
+game.state.add('main', MainState)
 
 // Start the state to actually start the game
-game.state.start('main');
+game.state.start('main')
--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,36 @@
-const path = require('path');
+const path = require('path')
 
 // Phaser webpack config
-const phaserModule = path.join(__dirname, '/node_modules/phaser/');
-const phaser = path.join(phaserModule, 'build/custom/phaser-split.js');
-const pixi = path.join(phaserModule, 'build/custom/pixi.js');
-const p2 = path.join(phaserModule, 'build/custom/p2.js');
+const phaserModule = path.join(__dirname, '/node_modules/phaser/')
+const phaser = path.join(phaserModule, 'build/custom/phaser-split.js')
+const pixi = path.join(phaserModule, 'build/custom/pixi.js')
+const p2 = path.join(phaserModule, 'build/custom/p2.js')
 
 module.exports = {
-	entry: [
-		'babel-polyfill',
-		path.join(__dirname, 'main.js'),
-		'phaser', 'pixi', 'p2'
-	],
-	output: {
-		filename: 'bundle.js'
-	},
-	module: {
-		loaders: [
-			{test: /\.js$/, loader: 'babel-loader', exclude: /(node_modules|bower_components)/},
-			{test: /pixi\.js/, loader: 'expose?PIXI'},
-			{test: /p2\.js/, loader: 'expose?p2'},
-			{test: /phaser-split\.js$/, loader: 'expose?Phaser'}
-		]
-	},
-	node: {
-		fs: 'empty'
-	},
-	resolve: {
-		alias: {
-			phaser,
-			pixi,
-			p2
-		}
-	}
-};
+  entry: [
+    'babel-polyfill',
+    path.join(__dirname, 'main.js'),
+    'phaser', 'pixi', 'p2'
+  ],
+  output: {
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {test: /\.js$/, loader: 'babel-loader', exclude: /(node_modules|bower_components)/},
+      {test: /pixi\.js/, loader: 'expose?PIXI'},
+      {test: /p2\.js/, loader: 'expose?p2'},
+      {test: /phaser-split\.js$/, loader: 'expose?Phaser'}
+    ]
+  },
+  node: {
+    fs: 'empty'
+  },
+  resolve: {
+    alias: {
+      phaser,
+      pixi,
+      p2
+    }
+  }
+}
